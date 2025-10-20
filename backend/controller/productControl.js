e2e90c61b8bf2f4a096e590117c6b4f4e2617e47
imanudevv <anuanudev100@gmail.com>
2025-10-20 21:18:52 +0530

    2:15:20

--- backend/controller/productControl.js
+++ backend/controller/productControl.js
@@ -36,5 +36,40 @@ if(!product){
     success:true,
     product
   })
-  
 }
+
+// 4️⃣4️ DELETE PRODUCT
+
+export const deleteProduct = async(req,res)=>{
+   let  product = await Product.findById(req.params.id);
+   if(!product){
+  return res.status(500).json({
+    success:false,
+    messege:"product not found"
+  })
+}
+product=await Product.findByIdAndDelete(req.params.id);
+ res.status(200).json({
+    success:true,
+    messege:"Product Deleted Successfully"
+  })
+}
+
+
+// 5️⃣ ACCESSING SINGLE PRODUCT
+
+export const getSingleProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product Not Found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
--- backend/routes/prodectRoute.js
+++ backend/routes/prodectRoute.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createProducts, getAllProducts, updateProducts } from '../controller/productControl.js';
+import { createProducts, deleteProduct, getAllProducts, getSingleProduct, updateProducts } from '../controller/productControl.js';
 const router = express.Router();
 
 //routes
-router.route("/products").get(getAllProducts).post(createProducts);
-router.route("/product/:id").put(updateProducts);
+router.route("/products")
+.get(getAllProducts)
+.post(createProducts);
+router.route("/product/:id")
+.put(updateProducts)
+.delete(deleteProduct).get(getSingleProduct)
 
 export default router;
