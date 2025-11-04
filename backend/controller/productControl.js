2d62e3a984f0d3454d35d7fb62280aa6aa1b75c5
imanudevv <anuanudev100@gmail.com>
2025-11-04 12:09:51 +0530

    3:41:52

--- backend/controller/productControl.js
+++ backend/controller/productControl.js
@@ -17,7 +17,7 @@ export const createProducts =handleAsyncError (async (req, res,next) => {
 
 // 2️⃣ GET ALL PRODUCTS
 export const getAllProducts = handleAsyncError(async (req, res, next) => {
-  const resultPerPage = 3;
+  const resultPerPage = 9;
   const apiFeatures = new APIFunctionality(Product.find(), req.query)
     .search()
     .filter();
@@ -25,13 +25,32 @@ export const getAllProducts = handleAsyncError(async (req, res, next) => {
   // getting filtered query before pagination
   const filteredQuery = apiFeatures.query.clone();
   const productCount = await filteredQuery.countDocuments();
-  console.log(productCount);
 
-  const products = await apiFeatures.query;
+
+// Calculate totalPages based on filtered count
+const totalPages = Math.ceil(productCount / resultPerPage);
+const page = Number(req.query.page) || 1;
+
+if (page > totalPages && productCount > 0) {
+  return next(new HandleError("This page doesn't exist", 404));
+}
+
+
+//Apply pagination
+apiFeatures.pagination(resultPerPage)
+const products = await apiFeatures.query;
+
+if (!products || products.length===0){
+  return next (new HandleError("No Product Found",404))
+}
+
   res.status(200).json({
     success: true,
     products,
-    productCount
+    productCount,
+    resultPerPage,
+    totalPages,
+    currentPage:page
   });
 });
 
