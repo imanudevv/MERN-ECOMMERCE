30c4b3b07903c3cde78a490520e095ab629ad8ae
imanudevv <anuanudev100@gmail.com>
2025-11-05 21:46:31 +0530

    4:23:24

---
+++ backend/controller/userController.js
@@ -0,0 +1,23 @@
+
+import handleAsyncError from '../middleware/handleAsyncError.js'
+import User from  '../models/userModel.js';
+export const registerUser=handleAsyncError(async(req,res, next)=>{
+   const {name,email,password}=req.body;
+
+   const user=await User.create({
+    name,
+    email,
+    password,
+    avatar:{
+        public_id:"This is temp id",
+        url:"This is temp url"
+    }
+   })
+
+   const token=user.getJWTToken();
+   res.status(201).json({
+    success:true,
+    user,
+    token
+   })
+})
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,17 +1,61 @@
 import mongoose from "mongoose";
 import validator from 'validator';
+import bycryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-const userScema= new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true, "Please Enter your name"],
-        maxLength:[25,"Invalid name. Plese enter a name with fewer than 25 characters"],
-        minLength:[3,"Name Should contain more then 3 characters"]
+const userScema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Enter your name"],
+        maxLength: [25, "Invalid name. Plese enter a name with fewer than 25 characters"],
+        minLength: [3, "Name Should contain more then 3 characters"]
     },
-    email:{
+    email: {
+        type: String,
+        required: [true, "Please Enter your email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter valid"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter Your password"],
+        minLength: [8, "password should be greater than 8 characters"],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    role:{
         type:String,
-        required:[true,"Please Enter Your password"],
-        unique:true,
-        minLength:[validator.isEmail,"Please enter valid"]
+        default:"user"
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+},{timestamps:true})
+
+//Password Hashing
+userScema.pre("save",async function(){
+    this.password=await bycryptjs.hash(this.password,10)
+    //1st -updating profile(name , email, image)--hashed again❌
+    //2nd - Update password ✅
+    if (!this.isModified("password")){
+        return next();
     }
 })
+
+userScema.methods.getJWTToken=function(){
+    return jwt.sign({id:this._id},process.env.
+        JWT_SECRET_KEY,{
+            expiresIn:process.env.JWT_EXPIRE
+        }
+    )
+}
+
+export default mongoose.model("User",userScema)
