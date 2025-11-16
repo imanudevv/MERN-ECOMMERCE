2709a3b457611620cc816d1f0cd0ffb654b62733
imanudevv <anuanudev100@gmail.com>
2025-11-16 20:25:27 +0530

    16/11/25

--- backend/controller/productControl.js
+++ backend/controller/productControl.js
@@ -128,3 +128,4 @@ export const getAdminProducts = handleAsyncError(async (req, res, next) => {
   const products = await Product.find();
   res.status(200).json({ success: true, products });
 });
+//
