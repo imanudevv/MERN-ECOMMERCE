43a4d7d8e3147f01741b8960321b9d84b33e9edb
imanudevv <anuanudev100@gmail.com>
2025-11-27 21:03:59 +0530

    10:8

---
+++ frontent/src/components/ImageSlider.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import '../componentStyles/ImageSlider.css'
+
+const images = [
+  "./images/img1.jpg",
+  "./images/img2.jpg",
+  "./images/img3.jpg",
+  "./images/img4.png"
+]
+
+function ImageSlider() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="image-slider-container">
+      <div
+        className="slider-images"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <div className="slider-item" key={index}>
+            <img src={image} alt={`Slide ${index + 1}`} />
+          </div>
+        ))}
+      </div>
+
+      <div className="slider-dots">
+        <span className='dot' />
+      </div>
+    </div>
+  )
+}
+
+export default ImageSlider
--- frontent/src/pages/Home.jsx
+++ frontent/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import '../pageStyles/Home.css'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import ImageSlider from '../components/ImageSlider'
 
 function Home() {
   return (
     <>
     <Navbar/>
+    <ImageSlider/>
     <div className="home-container">
       <h2 className='home-heading'>Trending Now</h2>
       </div>
